6c078b29cceb74977ada927593d6b94b2f2caf1b
l-fan3 <lfan3@berklee.edu>
2025-09-16 18:22:50 -0700

    First Upload

---
+++ FanLiam_p5Js_week2_F25/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+background(100, 150, 255, 50);
+noStroke();
+circle (windowWidth/2, windowHeight/2, 200);
+}
